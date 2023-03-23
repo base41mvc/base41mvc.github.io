bac7dad875cffb307ed41863c494225ae2b35d6c
base41mvc <base41mvc@gmail.com>
2023-03-23 07:47:32 -0600

    nuevo

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCFgdR1FA28rYSuWFNSjlSIMEqEfvGAJ8g",
+    authDomain: "base41mvc1.firebaseapp.com",
+    projectId: "base41mvc1",
+    storageBucket: "base41mvc1.appspot.com",
+    messagingSenderId: "852587486084",
+    appId: "1:852587486084:web:0b5fcc0f3375c1ef3adbd1",
+    measurementId: "G-NW5NVE217M"
+});
